0c3813581a9d73daa49ab3874f99a5838962af3a
Николай <fetissov.n@gmail.com>
2017-06-15 11:55:03 +0300

    static methods made for Utils class

--- src/main/java/com/nick/hateportal/utils/Utils.java
+++ src/main/java/com/nick/hateportal/utils/Utils.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class Utils {
 
-    public String stringToLowerCase(String s){
+    public static String stringToLowerCase(String s){
         String newString = s.toLowerCase();
         return newString;
     }
 
-    public String firstLetterToHighCase(String s){
+    public static String firstLetterToHighCase(String s){
         String str = stringToLowerCase(s);
 
         Character firstChar = str.charAt(0);
