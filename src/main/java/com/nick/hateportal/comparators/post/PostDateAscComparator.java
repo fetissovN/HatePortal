455246de64f41f15d630d0a4f241193af6d394ab
Николай <fetissov.n@gmail.com>
2017-06-15 12:20:46 +0300

    Date comparator refactoring

--- src/main/java/com/nick/hateportal/comparators/post/PostDateAscComparator.java
+++ src/main/java/com/nick/hateportal/comparators/post/PostDateAscComparator.java
@@ -4,17 +4,24 @@ import com.nick.hateportal.entity.Post;
 import org.springframework.stereotype.Component;
 
 import java.util.Comparator;
+import java.util.Date;
 
 @Component
 public class PostDateAscComparator<T extends Post> implements Comparator<T> {
     @Override
     public int compare(T o1, T o2) {
-        if (o1.getPostDate().before(o2.getPostDate())){
+        Date o1Date = o1.getPostDate();
+        Date o2Date = o2.getPostDate();
+        if (o1Date!=null || o2Date!=null){
+            if (o1Date.before(o2Date)){
+                return -1;
+            } else if (o1Date.after(o2Date)){
+                return 1;
+            } else {
+                return 0;
+            }
+        }else {
             return -1;
-        } else if (o1.getPostDate().after(o2.getPostDate())){
-            return 1;
-        } else {
-            return 0;
         }
     }
 }
